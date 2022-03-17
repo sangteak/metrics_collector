79298c9642bcb81bdfccb0ca24ea32531794932a
NMPC\sangtakeg <sangtakeg@naver.com>
2022-03-17 18:22:57 +0900

    modified perf_executor sample

--- include/perf_executor.h
+++ include/perf_executor.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <thread>
-#include <mutex>
 #include <array>
 #include "perf_interface.h"
 #include "mmf.h"
@@ -16,108 +15,131 @@
 // 4) 필요한 위치에서 PerfTask에 수집 데이터 저장 후 수집기로 전달
 //
 // ** 공유 메모리에 주기적으로 데이터를 갱신하기 때문에 perfmon 또는 기타 툴을 이용해 해당 데이터를 읽어 기록해야함.
-
-/*
-1) 서비스 생성
-class PerfService : public IPerfService
-{
-public:
-	enum eId : int32_t
-	{
-		SEND_PACKET = 1,
-		SEND_PACKET_AMOUNT = 2,
-	};
-
-	virtual bool Collect(const int32_t& id, const std::shared_ptr<PerfTask>& msg)
-	{
-		switch (id)
-		{
-		case eId::SEND_PACKET:
-			++m_sendPacketCount;
-			break;
-
-		case eId::SEND_PACKET_AMOUNT:
-			m_sendPacketAmount += msg->get<0>();
-			break;
-
-		default:
-			return false;
-		}
-
-		return true;
-	}
-
-	virtual bool Marshel(std::vector<int32_t>& result)
-	{
-		result.emplace_back(m_sendPacketCount);
-		result.emplace_back(m_sendPacketAmount);
-	}
-
-	virtual void Reset()
-	{
-		m_sendPacketCount = 0;
-		m_sendPacketAmount = 0;
-	}
-
-private:
-	int32_t m_sendPacketCount = 0;
-	int32_t m_sendPacketAmount = 0;
-};
-
-2) PerfExecutor 생성 및 시작
-
-// 객체 선언
-nmsp::perf_collector g_collector;
-
-void main(void)
-{
-	PerfService service;
-	PerfExecutor perfExecutor;
-	perfExecutor.Start("Global\\sample_mmf", 
-	// 수집기 시작!
-	if (false == g_collector.Start("sample_mmf", std::static_pointer_cast<IPerfService>(std::make_shared<PerfService>()), 1000, PerfExecutor::eThreadType::WORKER_THREAD)
-	{
-		return;
-	}
-
-	// 주기적 데이터 입력을 위한 쓰레드 생성
-	auto producer = std::thread([&perfExecutor]() {
-
-		nmsp::_stop_watch_t endTimer;
-
-		while ((3600 * 1000) > endTimer.Lap())
-		{
-		// 수집기에 등록
-			perfExecutor.Put(PerfService::eId::SEND_PACKET_AMOUNT, std::make_shared<PerfTask>(1000)));
-
-			std::this_thread::sleep(std::chrono::milliseconds(10);
-		}
-	});
-
-	producer.join();
-
-	std::this_thread::sleep(std::chrono::seconds(30);
-
-	std::cout << "terminate" << std::endl;
-
-	getchar();
-}
-*/
-
-//#include <tbb/concurrent_queue.h>
-
-// binary 구성을 위한 streambuf
-// 사용법은 boost::asio::streambuf 문서 참고
-// class perf_result : public boost::asio::streambuf
+// 
+// [샘플 코드]
+// class PerfQueue : public IPerfQueue
 // {
 // public:
-// 	using __super_t = boost::asio::streambuf;
-// 
-// 	const void* get_pointer()
+// 	virtual bool TryPop(_element_t& task)
 // 	{
-// 		return boost::asio::buffer_cast<const void*>(__super_t::data());
+// 		std::lock_guard<std::mutex> lock(m_mutex);
+// 		if (true == m_queue.empty())
+// 		{
+// 			return false;
+// 		}
+// 
+// 		task = m_queue.front();
+// 		m_queue.pop_front();
+// 
+// 		return true;
 // 	}
+// 
+// 	virtual void Put(_element_t&& task)
+// 	{
+// 		std::lock_guard<std::mutex> lock(m_mutex);
+// 		m_queue.push_back(std::forward<_element_t>(task));
+// 	}
+// 
+// private:
+// 	std::mutex m_mutex;
+// 	std::deque<_element_t> m_queue;
 // };
+// 
+// class PerfService : public IPerfService
+// {
+// public:
+// 	enum eId : int32_t
+// 	{
+// 		SEND_PACKET = 1,
+// 		SEND_PACKET_AMOUNT = 2,
+// 	};
+// 
+// 	//virtual bool Collect(const int32_t& id, const std::shared_ptr<PerfTask<>>& msg)
+// 	virtual bool Collect(const int32_t& id, _task_pack_t&& task) override
+// 	{
+// 		switch (id)
+// 		{
+// 		case eId::SEND_PACKET:
+// 			++m_sendPacketCount;
+// 			break;
+// 
+// 		case eId::SEND_PACKET_AMOUNT:
+// 			m_sendPacketAmount += task.get<0>();
+// 			break;
+// 
+// 		default:
+// 			return false;
+// 		}
+// 
+// 		std::cout << "count=" << m_sendPacketCount << ", amount=" << m_sendPacketAmount << std::endl;
+// 	
+// 		return true;
+// 	}
+// 
+// 	virtual bool Marshel(std::vector<int32_t>& result) override
+// 	{
+// 		result.emplace_back(m_sendPacketCount);
+// 		result.emplace_back(m_sendPacketAmount);
+// 
+// 		return true;
+// 	}
+// 
+// 	virtual void Reset() override
+// 	{
+// 		m_sendPacketCount = 0;
+// 		m_sendPacketAmount = 0;
+// 	}
+// 
+// private:
+// 	int32_t m_sendPacketCount = 0;
+// 	int32_t m_sendPacketAmount = 0;
+// };
+// 
+// int main() 
+// {	
+// 	PerfService service;
+// 	using _perf_executor_t = PerfExecutor<PerfQueue>;
+// 	
+// 	_perf_executor_t perfExecutor;
+// 	perfExecutor.Run(
+// 		"Global\\sample_mmf",
+// 		std::static_pointer_cast<IPerfService>(std::make_shared<PerfService>()),
+// 		1000,
+// 		_perf_executor_t::eThreadType::WORKER_THREAD
+// 	);
+// 
+// 	// 주기적 데이터 입력을 위한 쓰레드 생성
+// 	auto producer = std::thread([&perfExecutor]() {
+// 
+// 		auto startTime = std::chrono::steady_clock::now();
+// 		auto endTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - startTime).count();
+// 
+// 		while (6000 > endTime)
+// 		{
+// 			_task_pack_t task(1000);
+// 
+// 			// 수집기에 등록
+// 			perfExecutor.Put(PerfService::eId::SEND_PACKET_AMOUNT, std::move(task));
+// 
+// 			std::this_thread::sleep_for(std::chrono::seconds(1));
+// 
+// 			endTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - startTime).count();
+// 		}
+// 	});
+// 
+// 	producer.join();
+// 
+// 	std::this_thread::sleep_for(std::chrono::seconds(3));
+// 
+// 	std::cout << "terminate" << std::endl;
+// 
+// 	perfExecutor.Stop();
+// 
+// 	getchar();
+// 	
+// 	return 0;
+// }
+
 template <typename Queue>
 class PerfExecutor
 {
